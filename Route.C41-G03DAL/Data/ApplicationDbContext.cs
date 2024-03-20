3fd5a40d1c72af049348afb8bd2a20e807d37066
Nada Mohamed <nada971990@gmail.com>
2024-03-20 12:50:12 +0200

    DAL for Department Module

---
+++ Route.C41-G03DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41_G03DAL.Data.Configurations;
+using Route.C41_G03DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41_G03DAL.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        
+          => optionsBuilder.UseSqlServer("Server=.;Database=MVCApplication-G03;TrustedConnection=True") ;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ Route.C41-G03DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41_G03DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; } //primary key 
+
+        public string Code { get; set; }
+        
+        public string Name { get; set; }
+
+        public DateTime DateOfCereation{ get; set; }= DateTime.Now;
+    }
+}
