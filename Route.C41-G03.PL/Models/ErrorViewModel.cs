02b23fb5b61cf853bae35c52246ed0cab25fe790
Eng khaled <Eng khaled@DESKTOP-U9G3PIN>
2024-03-20 10:25:57 +0200

    Add project files.

---
+++ Route.C41-G03.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route.C41_G03.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
