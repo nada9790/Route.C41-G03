243faf76c9a6d8694994977b2e6c157e38f38446
Nada Mohamed <nada971990@gmail.com>
2024-03-21 16:07:13 +0200

    create department controller

---
+++ Route.C41-G03.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Route.C41_G03.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
