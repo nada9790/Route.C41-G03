522d9a3784707a7d7dd3052e6084f7e4e23a5f78
Nada Mohamed <nada971990@gmail.com>
2024-03-30 23:28:07 +0200

    UnitOfWork

---
+++ Route.C41-G03.BLL/Interface/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41_G03.BLL.Interface
+{
+    internal class IEmployeeRepository
+    {
+        IQueryable GetByAddress(string address);
+        IQueryable SearchByName(string Name);
+    }
+}
---
+++ Route.C41-G03.BLL/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Route.C41_G03.BLL.Interface;
+using Route.C41_G03DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41_G03.BLL.Repositories
+{
+    internal class UnitOfWork:IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            EmployeeRepository = new EmployeeRepository(dbContext);
+            DepartmentRepository = new DepartmentRepository(dbContext);
+            this.dbContext = dbContext;
+        }
+        public IEmployeeRepository EmployeeRepository { get; set; }
+        public IDepartmentRepository DepartmentRepository { get; set; }
+
+        public int Complete()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        => dbContext.Dispose();
+    }
+}
