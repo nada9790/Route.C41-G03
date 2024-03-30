858b5f9ded5446f7ef767f091afdeeb11f938356
NadaMohamed90 <132035134+nada9790@users.noreply.github.com>
2024-03-30 23:50:51 +0200

    Add files via upload

---
+++ Route.C41-G03.BLL/Interface/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41_G03.BLL.Interface
+{
+    internal interface IUnitOfWork
+    {
+        public IEmployeeRepository EmployeeRepository { get; }
+        public IDepartmentRepository DepartmentRepository { get; }
+
+        int Complete();
+    }
+}
